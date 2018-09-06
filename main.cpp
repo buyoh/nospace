c1788a2303d890f0dd1aa0b5f42f3081634045a8
buyoh <japanjetone@live.jp>
2018-09-07 05:20:11 +0900

    add Token

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@ using namespace std;
 using integer = int64_t;
 
 
+template<typename T, typename U>
+inline bool typeis(const U& a1) {
+    return typeid(a1) == typeid(T);
+}
+
+
 namespace WS {
 
     enum struct Chr {
@@ -87,9 +93,160 @@ namespace WS {
 
 namespace Parser {
 
-
     struct ParseException : runtime_error {
         ParseException(const char* msg = "") :runtime_error(msg) { }
+    };
+
+
+    struct Token {
+        virtual ~Token(){}
+        virtual bool operator==(const Token& t) const = 0;
+    };
+
+
+    class TokenStream {
+        vector<unique_ptr<Token>> tokens_;
+        size_t ptr;
+    public:
+        TokenStream():ptr(0){}
+        TokenStream(vector<unique_ptr<Token>>&& _tokens) :tokens_(move(_tokens)), ptr(0) { }
+
+        const Token& get() { return *tokens_[ptr++]; }
+        TokenStream& seekg(size_t pos) { ptr = pos; return *this; }
+        const Token& peek() const { return *tokens_[ptr]; }
+        const bool eof() const { return ptr >= tokens_.size(); }
+    };
+
+
+    class TokenInteger : public Token {
+        const integer value_;
+    public:
+        TokenInteger(integer _val):value_(_val){}
+
+        inline integer get() const { return value_; }
+
+        inline bool operator==(const TokenInteger& t) const {
+            return value_ == t.value_;
+        }
+        inline bool operator==(const Token& t) const {
+            try {
+                return operator==(dynamic_cast<const TokenInteger&>(t));
+            }
+            catch (bad_cast) {
+                return false;
+            }
+        }
+    };
+
+
+    class TokenKeyword : public Token {
+        const string keyword_;
+    public:
+        TokenKeyword(string _keyword) :keyword_(_keyword) {}
+
+        inline bool operator==(const TokenKeyword& t) const {
+            return keyword_ == t.keyword_;
+        }
+        inline bool operator==(const string& str) const {
+            return keyword_ == str;
+        }
+        inline bool operator==(const Token& t) const {
+            try {
+                return operator==(dynamic_cast<const TokenKeyword&>(t));
+            }
+            catch (bad_cast) {
+                return false;
+            }
+        }
+    };
+
+
+    class TokenSymbol : public Token {
+        const char _symbol[4];
+    public:
+        TokenSymbol(char _s1) :_symbol{_s1,0,0,0} {}
+        TokenSymbol(char _s1, char _s2) :_symbol{ _s1,_s2,0 } {}
+        TokenSymbol(char _s1, char _s2, char _s3) :_symbol{ _s1,_s2,_s3,0 } {}
+
+        inline bool operator==(const TokenSymbol& t) const {
+            return strcmp(_symbol, t._symbol) == 0;
+        }
+        inline bool operator==(char c) const {
+            return _symbol[0] == c && _symbol[1] == 0;
+        }
+        inline bool operator==(const string& str) const {
+            return _symbol == str;
+        }
+        inline bool operator==(const Token& t) const {
+            try {
+                return operator==(dynamic_cast<const TokenSymbol&>(t));
+            }
+            catch (bad_cast) {
+                return false;
+            }
+        }
+    };
+
+
+    inline bool isValidSymbol(char c) {
+        static bool f[] = {0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0 };
+        return (32 <= c && c < 128) ? f[c - 32] : false;
+    }
+
+
+
+    TokenInteger parseInteger(istream& is) {
+        integer var = 0; integer sign = 1;
+        int cc = is.peek();
+        for (; (cc < '0' || '9' < cc) && cc != istream::traits_type::eof(); is.get(), cc = is.peek())
+            ;// if (cc == '-') sign = -1;
+        for (; '0' <= cc && cc <= '9' && cc != istream::traits_type::eof(); is.get(), cc = is.peek())
+            var = (var << 3) + (var << 1) + cc - '0';
+        return TokenInteger(var * sign);
+    }
+
+
+    // TokenKeyword parseKeyword(istream& is)
+
+
+    TokenSymbol parseSymbol(istream& is) {
+        return TokenSymbol(is.get());
+    }
+
+
+    vector<unique_ptr<Token>> parseToTokens(istream& is) {
+        vector<unique_ptr<Token>> tokens;
+        tokens.reserve(24);
+
+        while (!is.eof()) {
+            int cc = is.peek();
+
+            if (isspace(cc)) {
+                is.get();
+                continue;
+            }
+            if (isdigit(cc)) {
+                tokens.emplace_back(new TokenInteger(parseInteger(is)));
+                continue;
+            }
+            if (isValidSymbol(cc)) {
+                tokens.emplace_back(new TokenSymbol(parseSymbol(is)));
+                continue;
+            }
+            is.get();
+        }
+        return move(tokens);
+    }
+
+}
+
+
+namespace Compiler {
+    using namespace Parser;
+
+
+    struct CompileException : runtime_error {
+        CompileException(const char* msg = "") :runtime_error(msg) { }
     };
 
 
@@ -148,109 +305,116 @@ namespace Parser {
 
     };
 
-    // isdigit
-
-    template<typename T> T getNumber(istream& is) {
-        T var = 0; T sign = 1;
-        int cc = is.peek();
-        for (; (cc < '0' || '9' < cc) && cc != istream::traits_type::eof(); is.get(), cc = is.peek())
-            if (cc == '-') sign = -1;
-        for (; '0' <= cc && cc <= '9'; is.get(), cc = is.peek())
-            var = (var << 3) + (var << 1) + cc - '0';
-        return var * sign;
-    }
-
-
-    ExpressionStatement getStatement(istream& is, int level) {
+       
+    ExpressionStatement getStatement(TokenStream& stream, int level) {
 
         unique_ptr<ExpressionStatement> root;
         unique_ptr<ExpressionStatement>* curr = &root;
 
+        integer intsign = 1;
         int stat = 0;
-        while (!is.eof()) {
-            int cc = is.peek();
-
-            if (isspace(cc)) {
-                is.get(); continue;
-            }
+        while (!stream.eof()) {
+            const Token& token = stream.peek();
 
             switch (stat) {
             case 0:
             case 2: {
-                if (isdigit(cc) || cc == '-') {
+                if (typeis<TokenInteger>(token)) {
                     assert(!(*curr));
                     if (level < 3) {
-                        curr->reset(new ExpressionStatement(getStatement(is, level + 1)));
+                        curr->reset(new ExpressionStatement(getStatement(stream, level + 1)));
                         stat = 1;
+                        break;
                     }
                     else if (level == 3) {
-                        if (isdigit(cc) || cc == '-') {
-                            integer val = getNumber<integer>(is);
-                            return ExpressionStatement(OperationMode::Integer, val);
+                        integer val = dynamic_cast<const TokenInteger&>(token).get();
+                        stream.get();
+                        return ExpressionStatement(OperationMode::Integer, intsign*val);
+                    }
+                }
+                if (typeis<TokenSymbol>(token)) {
+                    assert(!(*curr));
+                    if (level < 3) {
+                        curr->reset(new ExpressionStatement(getStatement(stream, level + 1)));
+                        stat = 1;
+                        break;
+                    }
+                    else if (level == 3) {
+                        const auto& tokenSymbol = dynamic_cast<const TokenSymbol&>(token);
+                        if (tokenSymbol == '-') {
+                            stream.get();
+                            intsign *= -1;
+                            break;
                         }
                     }
                 }
-                break;
+                throw CompileException();
             }
             case 1: {
-                if (cc == '+' || cc == '-') {
+                if (typeis<TokenSymbol>(token)) {
+                    const auto& tokenSymbol = dynamic_cast<const TokenSymbol&>(token);
 
-                    if (level < 1) {
-                        throw ParseException("internal error?");
+                    if (tokenSymbol == '+' || tokenSymbol == '-') {
+
+                        if (level < 1) {
+                            throw CompileException("internal error?");
+                        }
+                        else if (level == 1) {
+                            auto new_ex = new ExpressionStatement(
+                                tokenSymbol == '-' ? OperationMode::Subtract : OperationMode::Add);
+                            (*new_ex).args(0) = move(root);
+                            root.reset(new_ex);
+                            curr = &(*new_ex).args(1);
+                            stream.get();
+                            stat = 2;
+                            break;
+                        }
+                        else if (level > 1) {
+                            return move(*root);
+                        }
                     }
-                    else if (level == 1) {
-                        auto new_ex = new ExpressionStatement(
-                            cc == '-' ? OperationMode::Subtract : OperationMode::Add);
-                        (*new_ex).args(0) = move(root);
-                        root.reset(new_ex);
-                        curr = &(*new_ex).args(1);
-                        is.get();
-                        stat = 2;
-                    }
-                    else if (level > 1) {
-                        return move(*root);
+                    else if (tokenSymbol == '*' || tokenSymbol == '/' || tokenSymbol == '%') {
+                        if (level < 2) {
+                            throw CompileException("internal error?");
+                        }
+                        else if (level == 2) {
+                            auto new_ex = new ExpressionStatement(
+                                tokenSymbol == '/' ? OperationMode::Divide :
+                                tokenSymbol == '%' ? OperationMode::Modulo :
+                                OperationMode::Multiply);
+                            (*new_ex).args(0) = move(root);
+                            root.reset(new_ex);
+                            curr = &(*new_ex).args(1);
+                            stream.get();
+                            stat = 2;
+                            break;
+                        }
+                        else if (level > 2) {
+                            return move(*root);
+                        }
+
                     }
                 }
-                else if (cc == '*' || cc == '/' || cc == '%') {
-                    if (level < 2) {
-                        throw ParseException("internal error?");
-                    }
-                    else if (level == 2) {
-                        auto new_ex = new ExpressionStatement(
-                            cc == '/' ? OperationMode::Divide :
-                            cc == '%' ? OperationMode::Modulo :
-                            OperationMode::Multiply);
-                        (*new_ex).args(0) = move(root);
-                        root.reset(new_ex);
-                        curr = &(*new_ex).args(1);
-                        is.get();
-                        stat = 2;
-                    }
-                    else if (level > 2) {
-                        return move(*root);
-                    }
-                    
-                }
-                break;
+                throw CompileException();
             }
             }
         }
         if (stat == 1) return move(*root);
-        else throw ParseException();
+        else throw CompileException();
     }
 
 
-    ExpressionStatement getStatement(istream& is) {
-        return getStatement(is, 1);
+    ExpressionStatement getStatement(TokenStream& ts) {
+        return getStatement(ts, 1);
     }
 }
 
 
 //
 
-namespace Compiler {
+namespace Builder {
     using namespace WS;
-    using namespace Parser;
+    using namespace Compiler;
 
 
     struct OperatorException : runtime_error {
@@ -323,8 +487,11 @@ int main() {
     using namespace WS;
     using namespace Parser;
     using namespace Compiler;
+    using namespace Builder;
 
-    ExpressionStatement st = getStatement(cin);
+    TokenStream ts(parseToTokens(cin));
+
+    ExpressionStatement st = getStatement(ts);
 
     WhiteSpace code;
     convert_statement(code, st);
